aff3d4e5d580932c017e746957abf258466a85f2
张雪东 <zhangxuedong@zhangxuedongdeimac.local>
2016-04-15 10:06:23 +0800

    Initial Commit

---
+++ MoveUITableViewCellDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MoveUITableViewCellDemo
+//
+//  Created by 张雪东 on 16/4/15.
+//  Copyright © 2016年 张雪东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MoveUITableViewCellDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MoveUITableViewCellDemo
+//
+//  Created by 张雪东 on 16/4/15.
+//  Copyright © 2016年 张雪东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
